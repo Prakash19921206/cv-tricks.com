3b2eac39f3993f25b40c4e90e5a349fda351c951
Prakash19921206 <itsmeprakash01@gmail.com>
2018-05-11 09:35:58 +0530

    Update train.py

--- Tensorflow-tutorials/tutorial-2-image-classifier/train.py
+++ Tensorflow-tutorials/tutorial-2-image-classifier/train.py
@@ -178,31 +178,30 @@ total_iterations = 0
 saver = tf.train.Saver()
 def train(num_iteration):
     global total_iterations
-	
-	with tf.device('device:GPU:0'):
+    with tf.device('device:GPU:0'):
     
-		for i in range(total_iterations,
-					   total_iterations + num_iteration):
-
-			x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
-			x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(batch_size)
-
-			
-			feed_dict_tr = {x: x_batch,
-							   y_true: y_true_batch}
-			feed_dict_val = {x: x_valid_batch,
-								  y_true: y_valid_batch}
-
-			session.run(optimizer, feed_dict=feed_dict_tr)
-
-			if i % int(data.train.num_examples/batch_size) == 0: 
-				val_loss = session.run(cost, feed_dict=feed_dict_val)
-				epoch = int(i / int(data.train.num_examples/batch_size))    
-				
-				show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
-				saver.save(session, 'model/dogs-cats-model') 
-
-
-		total_iterations += num_iteration
+        for i in range(total_iterations,
+                       total_iterations + num_iteration):
+    
+            x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
+            x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(batch_size)
+    
+            
+            feed_dict_tr = {x: x_batch,
+                               y_true: y_true_batch}
+            feed_dict_val = {x: x_valid_batch,
+                                  y_true: y_valid_batch}
+    
+            session.run(optimizer, feed_dict=feed_dict_tr)
+    
+            if i % int(data.train.num_examples/batch_size) == 0: 
+                val_loss = session.run(cost, feed_dict=feed_dict_val)
+                epoch = int(i / int(data.train.num_examples/batch_size))    
+                
+                show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+                saver.save(session, 'model/dogs-cats-model') 
+    
+    
+        total_iterations += num_iteration
 
 train(num_iteration=3000)
